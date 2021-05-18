e540e79ff74abe310d20c54da77e4293421c51de
joranminjon <joranminjon@gmail.com>
2021-05-18 22:07:54 +0200

    our collection content

---
+++ src/assets/content.js
@@ -0,0 +1,26 @@
+export const ourCollection = [
+  {
+    img: "@/assets/home/desktop/image-gran-espresso.png",
+    title: "Gran Espresso",
+    paragraph:
+      "Light and flavorful blend with cocoa and black pepper for an intense experience",
+  },
+  {
+    img: "@/assets/home/desktop/image-planalto.png",
+    title: "Planalto",
+    paragraph:
+      "Brazilian dark roast with rich and velvety body, and hints of fruits and nuts",
+  },
+  {
+    img: "@/assets/home/desktop/image-piccollo.png",
+    title: "Piccollo",
+    paragraph:
+      "Mild and smooth blend featuring notes of toasted almond and dried cherry",
+  },
+  {
+    img: "@/assets/home/desktop/image-danche.png",
+    title: "Danche",
+    paragraph:
+      "Ethiopian hand-harvested blend densely packed with vibrant fruit notes",
+  },
+]
